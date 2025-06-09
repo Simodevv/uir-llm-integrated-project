1ee9e55d1e040546bc8a17104b5c404c25300e75
Simohamed <gosh@simo-software.com>
2025-06-09 12:44:47 +0100

    Added main project files

---
+++ README.md
@@ -0,0 +1,30 @@
+## 📈 AI Investment Agent
+This app is an AI-powered investment agent built with Agno's AI Agent framework that compares the performance of two stocks and generates detailed reports. By using Gemini with Yahoo Finance data, this app provides valuable insights to help you make informed investment decisions.
+
+### Features
+- Compare the performance of two stocks
+- Retrieve comprehensive company information
+- Get the latest company news and analyst recommendations
+- Get the latest company news and analyst recommendations
+
+### How to get Started?
+
+1. Clone the GitHub repository
+
+```bash
+git clone https://github.com/Simodevv/uir-llm-integrated-projects.git
+cd /uir-llm-integrated-projects
+```
+2. Install the required dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+3. Get your Gemini API Key
+
+- Sign up for a [Google AI Studio Account] and obtain your API key.
+
+4. Run the Streamlit App
+```bash
+streamlit run investment_agent.py
+```
---
+++ investment_agent.py
@@ -0,0 +1,100 @@
+import streamlit as st
+from agno.agent import Agent
+from agno.models.google import Gemini
+from agno.tools.yfinance import YFinanceTools
+from agno.storage.sqlite import SqliteStorage
+from agno.memory.v2.db.sqlite import SqliteMemoryDb
+from agno.memory.v2.memory import Memory
+from dotenv import load_dotenv
+import pandas as pd
+import os
+
+load_dotenv()
+
+st.title("AI Investment Agent 📈🤖")
+st.caption("An LLM powered agent that compares the performance of two stocks and generates detailed reports.")
+
+gemini_api_key = os.getenv("GOOGLE_API_KEY")
+
+storage = SqliteStorage(table_name="agent_sessions", db_file="tmp/agent.db")
+
+memory = Memory(
+    # Use any model for creating and managing memories
+    model=Gemini(),
+    # Store memories in a SQLite database
+    db=SqliteMemoryDb(table_name="user_memories", db_file="tmp/agent.db"),
+    # We disable deletion by default, enable it if needed
+    delete_memories=True,
+    clear_memories=True,
+)
+
+assistant = Agent(
+        model=Gemini(api_key=gemini_api_key),
+        tools=[
+            YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True)
+        ],
+        memory=memory,
+        enable_agentic_memory=True,
+        reasoning=True,
+        markdown=True,
+        storage=storage,
+        show_tool_calls=True,
+        enable_user_memories=True,
+        description="You are an investment analyst that researches stock prices, analyst recommendations, and stock fundamentals.",
+        instructions=[
+            "Format your response using markdown and use tables to display data where possible."
+        ],
+    )
+
+@st.cache_data
+def load_sp500_tickers():
+    url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+    table = pd.read_html(url)[0]
+    return table[['Symbol', 'Security', 'GICS Sector']]
+
+
+df = load_sp500_tickers()
+
+# Optional: Add search functionality
+search = st.text_input("🔍 Search company name or symbol:")
+
+if search:
+    filtered_df = df[df.apply(lambda row: search.lower() in row.astype(str).str.lower().to_string(), axis=1)]
+else:
+    filtered_df = df
+
+st.dataframe(filtered_df, use_container_width=True)
+
+
+col1, col2 = st.columns(2)
+with col1:
+    stock1 = st.text_input("Enter first stock symbol (e.g. AAPL)")
+with col2:
+    stock2 = st.text_input("Enter second stock symbol (e.g. MSFT)")
+    if stock1 or stock2:
+        if not stock1 or not stock2:
+            st.warning("Please enter both stock symbols.")
+        else:
+            st.subheader(f"Comparison Report: {stock1} vs {stock2}")
+            with st.spinner(f"Analyzing {stock1} and {stock2}..."):
+                query = f"Compare both the stocks - {stock1} and {stock2} and make a detailed report for an investment trying to invest and compare these stocks"
+                response = assistant.run(query, stream=False, show_full_reasoning=True)
+                assistant.print_response(query, stream=False, show_full_reasoning=True, stream_intermediate_steps=True)
+                #st.markdown(response.content)
+                with st.container():
+                    st.markdown(
+                        f"""
+                        <div style='
+                            background-color: #1e1e1e;
+                            padding: 20px;
+                            border-radius: 12px;
+                            box-shadow: 0 0 10px rgba(255, 255, 255, 0.1);
+                            text-align: left;
+                            font-size: 16px;
+                            line-height: 1.6;
+                            color: #e0e0e0;
+                        '>
+                            {response.content}</div>
+                        """,
+                        unsafe_allow_html=True
+                    )
